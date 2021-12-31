899389136b4661e2035ba66ed8236bcc594493fb
BeADre <beadre_wang@163.com>
2021-12-31 09:07:16 +0000

    Deploying to gh-page from @ haoziqaq/varlet@456601269552302b3e2dd9d31db9e6b077c86141 🚀

---
+++ assets/index.c12ede19.js
@@ -0,0 +1,1 @@
+import{P as n}from"./Pagination.22dfc9e0.js";n.install=function(a){a.component(n.name,n)};
--- assets/index.f702b3b8.js
+++
@@ -1,1 +0,0 @@
-import{P as n}from"./Pagination.bbd1d7bd.js";n.install=function(a){a.component(n.name,n)};
