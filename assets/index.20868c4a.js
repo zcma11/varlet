04a55726587ab60014d4a9c83667046125b3dcbe
haoziqaq <357229046@qq.com>
2022-01-19 13:53:10 +0000

    Deploying to gh-page from @ varletjs/varlet@a4634e0d468b865264381fa31acb2a4af87e2a9a 🚀

---
+++ assets/index.20868c4a.js
@@ -0,0 +1,1 @@
+import{P as n}from"./Pagination.548e6d5e.js";n.install=function(a){a.component(n.name,n)};
--- assets/index.706d6237.js
+++
@@ -1,1 +0,0 @@
-import{P as n}from"./Pagination.93f4fc91.js";n.install=function(a){a.component(n.name,n)};
