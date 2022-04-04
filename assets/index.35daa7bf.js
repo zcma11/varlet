55f945ce9c3995de6b35e3990cb9d6a5df931981
zcma11 <78355301+zcma11@users.noreply.github.com>
2022-04-04 08:38:46 +0000

    Deploying to gh-page from @ zcma11/varlet@95a9f252d9152caef2d0e97e705e556e84e75d7c 🚀

---
+++ assets/index.35daa7bf.js
@@ -0,0 +1,1 @@
+import{P as n}from"./Pagination.ad1f92a4.js";n.install=function(o){o.component(n.name,n)};
---
+++ assets/provide.b030969e.js
@@ -0,0 +1,1 @@
+import{f as E,g as _}from"./components.2481c71b.js";const n=Symbol("SWIPE_BIND_SWIPE_ITEM_KEY"),t=Symbol("SWIPE_COUNT_SWIPE_ITEM_KEY");function i(){const{childProviders:e,bindChildren:s}=E(n),{length:I}=_(t);return{length:I,swipeItems:e,bindSwipeItems:s}}export{n as S,t as a,i as u};
